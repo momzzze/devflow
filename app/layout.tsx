86927367b178da955a7dd76a0822e98d0c7c4256
nikola.ninov <momzzze@abv.bg>
2024-04-08 19:32:33 +0300

    add details route add ParseHTML to display the question ad form for answer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import React from "react";
 import "./globals.css";
+import "../styles/prism.css";
 import { ClerkProvider } from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,7 +4,7 @@
 import Question from "@/database/models/Question";
 import { connectToDatabase } from "../mongoose"
 import Tag from "@/database/models/Tag";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import { CreateQuestionParams, GetQuestionByIdParams, GetQuestionsParams } from "./shared.types";
 import User from "@/database/models/User";
 import { revalidatePath } from "next/cache";
 
@@ -49,5 +49,20 @@ export async function getQuestions(params: GetQuestionsParams) {
 
     } catch (error) {
         console.log(error);
+    }
+}
+
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+    try {
+        connectToDatabase();
+        const { questionId } = params;
+
+        const question = await Question.findById(questionId).populate({ path: 'tags', model: Tag, select: '_id name' }).populate({ path: 'author', model: User, select: '_id clerkId name picture' });
+
+        return question;
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
