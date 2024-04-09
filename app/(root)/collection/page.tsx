c5d3069f6d989929ae1b0be81c78e7f3dc1eda70
nikola.ninov <momzzze@abv.bg>
2024-04-09 11:20:55 +0300

    add saved questions

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,64 @@
+import QuestionCard from "@/components/cards/QuestionCard";
+import Filters from "@/components/shared/Filters";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchBar from "@/components/shared/search/LocalSearchBar";
+import { QuestionFilters } from "@/coonstants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const page = async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchBar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filters
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[175px]"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col space-x-1 shadow-none">
+        {result!.questions.length > 0 ? (
+          result!.questions.map((q) => (
+            <QuestionCard
+              key={q._id}
+              _id={q._id}
+              title={q.title}
+              tags={q.tags}
+              author={q.author}
+              upvotes={q.upvotes}
+              answers={q.answers}
+              views={q.views}
+              createdAt={q.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title=" There’s no saved question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+      discussion. our query could be the next big thing others learn from. Get
+      involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default page;
--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -17,7 +17,7 @@ interface Props {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -68,7 +68,7 @@ const QuestionCard = ({
         <Metric
           imgUrl="/assets/icons/like.svg"
           alt="Upvotes"
-          value={formatNumber(upvotes)}
+          value={formatNumber(upvotes.length)}
           title=" Votes"
           textStyles="small-medium text-dark400_light800"
         />
